c0ecc3e8f21cb5c9f5483f59b5d2db16f7e1742f
loretta_okoye@hotmail.com <loretta_okoye@hotmail.com>
2018-05-14 07:37:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,2 @@
 const app = "I don't do much."
 
-function destructivelyAppendKitten(name){
-  kittens.push(name);
-  return kittens
-}
-
-function destructivelyRemoveLastKitten(){
-  kittens.pop();
-  return kittens
-}
